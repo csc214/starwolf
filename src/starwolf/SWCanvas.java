c125076cb8d3b8a9a713ba4131bcee18db09d864
Timothy Haskins <oeathus@gmail.com>
2017-05-13 15:38:37 -0400

    Adds GUI elements for header info from cam

--- src/starwolf/SWCanvas.java
+++ src/starwolf/SWCanvas.java
@@ -2,6 +2,7 @@ package starwolf;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.image.PixelReader;
+import javafx.scene.paint.Color;
 
 /**
  * Created by oeathus on 4/27/17.
@@ -32,7 +33,7 @@ public class SWCanvas extends Canvas {
             PixelReader pixelReader = (PixelReader) buffer;
             for (int y = 0; y < height; ++y)
                 for (int x = 0; x < width; ++x)
-                    workingBuffer[x][y] = pixelReader.getArgb(x, y);
+                    workingBuffer[x][y] = (int)(pixelReader.getColor(x, y).getBrightness()*255);
         } else if(buffer instanceof int[][]){
             setWorkingBuffer((int[][]) buffer);
         }else if(buffer instanceof short[][]){
@@ -48,6 +49,6 @@ public class SWCanvas extends Canvas {
         this.getGraphicsContext2D().clearRect(0, 0, this.getWidth(), this.getHeight());
         for (int y = 0; y < getHeight(); ++y)
             for (int x = 0; x < getWidth(); ++x)
-                this.getGraphicsContext2D().getPixelWriter().setArgb(x, y, workingBuffer[x][y]);
+                this.getGraphicsContext2D().getPixelWriter().setColor(x, y, Color.grayRgb(workingBuffer[x][y]));
     }
 }
